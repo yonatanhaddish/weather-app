313276cb6255c6035c30fdf54d6b88826bd4ea12
Your Name <you@example.com>
2021-04-03 13:28:02 -0400

    current city info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
 var button= document.querySelector("#button-search");
 var citySelected= document.querySelector(".form-control");
+var currentName= document.querySelector("#current-city");
+var currentTemp= document.querySelector("#current-temp");
+var currentHum= document.querySelector("#current-hum");
+var currentWindSpeed= document.querySelector("#current-wind-speed");
 
 button.addEventListener('click',function() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" +citySelected.value+ "&units=metric&appid=5bb216c75f6edf82eda28a4bb30b0263").then(function(response) {
     return response.json();
 }).then(function(data) {
     console.log(data);
+    currentName.textContent= data.name;
+    currentTemp.textContent= data.main.temp + ' °C';
+    currentHum.textContent= data.main.humidity + ' %';
+    currentWindSpeed.textContent= Math.round(data.wind.speed*3.6) + ' km/h';
+
+    
     // console.log(data.city.name);
 }).catch(function(error) {
     console.log("Error");
+    alert("City name NOT FOUND. Insert a valid city name!");
 })
 });
 
